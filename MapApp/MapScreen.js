d850c8668fa7622baf267740a4f9c2332fc2da88
JarnoPauwels <71709292+JarnoPauwels@users.noreply.github.com>
2023-11-12 22:27:46 +0100

    Added

--- MapApp/MapScreen.js
+++ MapApp/MapScreen.js
@@ -2,44 +2,77 @@ import React, { useState, useEffect } from 'react';
 import { View, StyleSheet, Text } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
 import * as Location from 'expo-location';
+import { Magnetometer } from 'expo-sensors';
 import poiData from './poiData.json';
-import { GOOGLE_MAPS_API_KEY } from './secrets';
+import { GOOGLE_MAPS_API_KEY } from './ApiKey';
+
+const customMapStyle = [
+];
 
 const MapScreen = () => {
   const [location, setLocation] = useState(null);
+  const [compassData, setCompassData] = useState(null);
 
   const getLocation = async () => {
     let { status } = await Location.requestForegroundPermissionsAsync();
     if (status !== 'granted') {
-      setErrorMsg('Permission to access location was denied');
+      console.log('Permission to access location was denied');
       return;
     }
 
     let location = await Location.getCurrentPositionAsync({});
     setLocation(location.coords);
   };
-  
+
+  const requestMagnetometerPermission = async () => {
+    const { status } = await Magnetometer.getPermissionsAsync();
+    if (status !== 'granted') {
+      console.log('Permission to access the magnetometer sensor is denied.');
+    }
+  };
+
+  const updateCompassData = ({ x, y }) => {
+    const rotation = Math.atan2(y, x) * (270 / Math.PI);
+    setCompassData(rotation);
+  };
+
   useEffect(() => {
     getLocation();
-  }, []);
+    requestMagnetometerPermission();
+    Magnetometer.setUpdateInterval(500);
 
+    Magnetometer.addListener(updateCompassData);
+
+    return () => {
+      Magnetometer.removeAllListeners();
+    };
+  }, []);
 
   return (
     <View style={styles.container}>
       {location ? (
         <MapView
           style={styles.map}
-          initialRegion={{
-            latitude: location.latitude,
-            longitude: location.longitude,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
-          }}
+          // region={{
+          //   latitude: location.latitude,
+          //   longitude: location.longitude,
+          //   latitudeDelta: 0.001,
+          //   longitudeDelta: 0.001,
+          // }}
           showsUserLocation
-          followsUserLocation
-          provider="google" // Specify the map provider
-        //   customMapStyle={mapStyle} // Optional: If you have a custom map style
-          apikey={GOOGLE_MAPS_API_KEY} // Pass the API key here
+          zoomEnabled
+          provider="google"
+          customMapStyle={customMapStyle} 
+          camera={{
+            center: {
+              latitude: location.latitude,
+              longitude: location.longitude,
+            },
+            pitch: 0,
+            heading: compassData || 0, 
+            altitude: 1000, // IOS
+            zoom: 18, // GOOGLE
+          }}
         >
           {poiData.map((poi) => (
             <Marker
@@ -58,9 +91,6 @@ const MapScreen = () => {
     </View>
   );
 };
-
-// ... (rest of the code)
-
 
 const styles = StyleSheet.create({
   container: {
